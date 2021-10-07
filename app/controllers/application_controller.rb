f60ed34e0b56b425b4fdcda4bae884526f3efe29
Mateo Présiga Hidalgo <presigahidalgomateo@gmail.com>
2021-10-07 10:39:17 -0500

    Front: navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :user_signed_in?
     helper_method :name_user
+    helper_method :return_user
 
     def current_user
         User.find_by(id: session[:user_id])
@@ -19,5 +20,10 @@ class ApplicationController < ActionController::Base
 
     def authetificate_user!
         redirect_to login_path unless user_signed_in?
+    end
+
+    def return_user
+        @user = User.find_by(id: session[:user_id])
+        return @user    
     end
 end
